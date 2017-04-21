3a8e5f77c8fca287dc8ce9265dd242b287029312
Caleb Ely <le717.code@yahoo.com>
2017-04-21 19:23:55 -0400

    Finish gaps module

--- src/extras/gaps.py
+++ src/extras/gaps.py
@@ -20,16 +20,20 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 import bpy
 
 
-def init():
-    # Select all the mesh
+def main(objects, scale_val):
+    """Add small, uniform gaps between parts.
+
+    @param {List} objects - A list of all models in the scene.
+    @param {Number} scale_val - The amount a model should be scaled up or down.
+    """
     for cur_obj in objects:
-        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.object.select_all(action="DESELECT")
         cur_obj.select = True
         bpy.context.scene.objects.active = cur_obj
 
-        # To change the width of the gaps, change the gapWidth variable
-        gapWidth = 0.007
-        objScale = cur_obj.scale * scale
+        # Compute the scale factor
+        gap_width = 0.007
+        obj_scale = cur_obj.scale * scale_val
         dim = cur_obj.dimensions
 
         # Checks whether the object isn't flat in a certain direction
@@ -38,18 +42,18 @@ def init():
         # the dimension so that the mesh shrinks a fixed distance
         # (determined by the gap_width and the scale of the object)
         # in every direction, creating a uniform gap.
-        scaleFac = {"x": 1, "y": 1, "z": 1}
+        scale_fac = {"x": 1, "y": 1, "z": 1}
 
         if dim.x != 0:
-            scaleFac["x"] = 1 - 2 * gapWidth * abs(objScale.x) / dim.x
+            scale_fac["x"] = 1 - 2 * gap_width * abs(obj_scale.x) / dim.x
         if dim.y != 0:
-            scaleFac["y"] = 1 - 2 * gapWidth * abs(objScale.y) / dim.y
+            scale_fac["y"] = 1 - 2 * gap_width * abs(obj_scale.y) / dim.y
         if dim.z != 0:
-            scaleFac["z"] = 1 - 2 * gapWidth * abs(objScale.z) / dim.z
+            scale_fac["z"] = 1 - 2 * gap_width * abs(obj_scale.z) / dim.z
 
-        bpy.context.object.scale[0] *= scaleFac["x"]
-        bpy.context.object.scale[1] *= scaleFac["y"]
-        bpy.context.object.scale[2] *= scaleFac["z"]
+        bpy.context.object.scale[0] *= scale_fac["x"]
+        bpy.context.object.scale[1] *= scale_fac["y"]
+        bpy.context.object.scale[2] *= scale_fac["z"]
 
         bpy.ops.object.transform_apply(scale=True)
-        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.object.select_all(action="DESELECT")
